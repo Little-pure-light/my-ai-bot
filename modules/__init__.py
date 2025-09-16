75082434d698f54392996b25e85ab6fbc38a5668
Little-pure-light <purelighthappy@gmail.com>
2025-09-17 00:31:08 +0800

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,11 +1,4 @@
 # modules/__init__.py
-"""
-小宸光 Bot 模組包
-模組化架構實現
-"""
+from .file_handler import FileHandler
 
-__version__ = "1.0.0"
-__author__ = "小宸光團隊"
-
-# 這個檔案告訴 Python 這個資料夾是一個 "包"
-# 讓我們可以使用: from modules.file_handler import FileHandler
+__all__ = ['FileHandler']
