192ca85666140411a19be226fee6d7946827882c
Little-pure-light <purelighthappy@gmail.com>
2025-09-16 22:44:27 +0800

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,11 @@
+# modules/__init__.py
+"""
+小宸光 Bot 模組包
+模組化架構實現
+"""
+
+__version__ = "1.0.0"
+__author__ = "小宸光團隊"
+
+# 這個檔案告訴 Python 這個資料夾是一個 "包"
+# 讓我們可以使用: from modules.file_handler import FileHandler
