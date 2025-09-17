4d43abc325eaf51a15822bbf405c3f9e721b32df
Little-pure-light <purelighthappy@gmail.com>
2025-09-17 13:51:24 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,7 @@ from telegram import Update
 from openai import OpenAI, APIError
 from supabase import create_client, Client
 from dotenv import load_dotenv
-from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes
-
+from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes, MessageHandler, filters
 from modules.file_handler import handle_file, download_full_file
 # 載入環境變量
 load_dotenv()
