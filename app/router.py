d71ba9d8b95c6bb716b84a7a4cadb07c711b0e09
Little-pure-light <purelighthappy@gmail.com>
2025-09-20 00:19:37 +0800

    Update router.py

--- app/router.py
+++ app/router.py
@@ -2,3 +2,5 @@ from telegram.ext import Updater, MessageHandler, Filters
 from app.config import settings
 from handlers.files import handle_file
 from handlers.text import handle_text
+from .handllers import files
+dp.add_handler(MessageHandler(filters.Document.ALL, files.handle_file))
