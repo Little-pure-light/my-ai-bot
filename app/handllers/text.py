73d2c19c78257c65b984fb170ee18bf4eac831a5
Little-pure-light <purelighthappy@gmail.com>
2025-09-20 01:33:32 +0800

    Delete app directory

--- app/handllers/text.py
+++
@@ -1,9 +0,0 @@
-from aiogram import types, Dispatcher
-
-# 處理一般文字訊息
-async def handle_text(message: types.Message):
-    await message.answer(f"收到文字訊息：{message.text}")
-
-# 註冊
-def register(dp: Dispatcher):
-    dp.register_message_handler(handle_text, content_types=['text'])
