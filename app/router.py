fd63eacc8502a580f8b4dc03853993a4999d7803
Little-pure-light <purelighthappy@gmail.com>
2025-09-20 01:14:46 +0800

    Update router.py

--- app/router.py
+++ app/router.py
@@ -1,15 +1,17 @@
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import MessageHandler, filters
+from . import files
 from app.config import settings
 from handlers.files import handle_file
 from handlers.text import handle_text
 from .handllers import files
 dp.add_handler(MessageHandler(filters.Document.ALL, files.handle_file))
 
-def register_handlers(application: Application):
+def register_handlers(app):
     # 文件上傳
-    application.add_handler(MessageHandler(filters.Document.ALL, files.handle_file))
+    app.add_handler(MessageHandler(filters.Document.ALL, files.handle_file))
     # 圖片上傳
-    application.add_handler(MessageHandler(filters.PHOTO, files.handle_image))
+    app.add_handler(MessageHandler(filters.PHOTO, files.handle_image))
+
 
 
 
