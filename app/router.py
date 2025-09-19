33e80110ed99b10da61558ac8b6ccbe71b9e1d0d
Little-pure-light <purelighthappy@gmail.com>
2025-09-20 00:32:26 +0800

    Update router.py

--- app/router.py
+++ app/router.py
@@ -4,3 +4,11 @@ from handlers.files import handle_file
 from handlers.text import handle_text
 from .handllers import files
 dp.add_handler(MessageHandler(filters.Document.ALL, files.handle_file))
+
+def register_handlers(application: Application):
+    # 文件上傳
+    application.add_handler(MessageHandler(filters.Document.ALL, files.handle_file))
+    # 圖片上傳
+    application.add_handler(MessageHandler(filters.PHOTO, files.handle_image))
+
+
